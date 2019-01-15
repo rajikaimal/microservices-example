6458ff86e2e2ac789650983078af3fdaa595d2cb
rajikaimal <inbox.rajika@gmail.com>
2019-01-15 23:15:55 +0530

    Add pkgs

--- orchestration/email-service/index.js
+++ orchestration/email-service/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const PORT = 3001;
+const PORT = 3001 || process.env.EMAIL_SERVICE;
+
+app.use(bodyParser.json());
 
 app.post("/api/v1/email", (req, res) => {
     // send email
